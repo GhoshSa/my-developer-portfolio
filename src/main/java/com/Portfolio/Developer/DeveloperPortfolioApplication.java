25084bc52746e0aec1c0f2b86e0216a0f51ed6db
Sayantan Ghosh <ghoshsayantan710@gmail.com>
2024-12-01 20:10:03 +0530

    Initial Commit

---
+++ src/main/java/com/Portfolio/Developer/DeveloperPortfolioApplication.java
@@ -0,0 +1,12 @@
+package com.Portfolio.Developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperPortfolioApplication.class, args);
+	}
+}

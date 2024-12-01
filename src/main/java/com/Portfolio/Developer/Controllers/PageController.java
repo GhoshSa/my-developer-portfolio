49cf75d1faa6ea45bec32346d571f88f8fcafb2f
Sayantan Ghosh <ghoshsayantan710@gmail.com>
2024-12-01 23:31:58 +0530

    Templates are added

--- src/main/java/com/Portfolio/Developer/Controllers/PageController.java
+++ src/main/java/com/Portfolio/Developer/Controllers/PageController.java
@@ -1,8 +1,27 @@
 package com.Portfolio.Developer.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    @GetMapping("/home")
+    public String Home() {
+        return "Home";
+    }
 
+    @GetMapping("/about")
+    public String About() {
+        return "About";
+    }
+
+    @GetMapping("/projects")
+    public String Projects() {
+        return "Projects";
+    }
+
+    @GetMapping("/contact")
+    public String Contact() {
+        return "Contact";
+    }
 }
